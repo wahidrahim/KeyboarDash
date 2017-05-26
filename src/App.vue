7d7a6e3af74b4bdcaf16e02f015a09275889d73f
Wahid Rahim <Wahid Rahim>
2017-05-26 16:26:06 -0400

    wip - spanned words with dumb highlight

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,79 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="container">
+        <div id="app">
+            <div class="words" v-html="htmlWords.join(' ')"></div>
+            <input type="text" v-model="input">
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'app',
+
+    data () {
+        return {
+            input: '',
+            words: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni sit repellendus deleniti facilis numquam illo aliquam accusamus sint assumenda repudiandae incidunt exercitationem qui doloribus vitae fuga, esse dolore iure. Rerum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum odio ipsa in velit officia quasi nulla placeat error! Illum atque excepturi, consequatur dolor eos consectetur est quam laborum iure et.'
+        }
+    },
+
+    computed: {
+        htmlWords() {
+            let nextWord = this.input.split(' ').length - 1
+            console.log(nextWord)
+
+            return this.words.split(' ').map((word, i) => {
+                if (i === nextWord) {
+                    return `<span class="next">${word}</span>`
+                } else {
+                    return `<span>${word}</span>`
+                }
+            })
+        }
     }
-  }
 }
 </script>
 
 <style lang="scss">
+@mixin font-style($unit) {
+    font-size: $unit;
+    word-spacing: $unit / 2;
+    line-height: $unit * 1.5;
+}
+
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .words {
+        border: 1px solid #DCDCDC;
+        padding: 5px 15px;
+        display: flex;
+        flex-wrap: wrap;
 
-h1, h2 {
-  font-weight: normal;
-}
+        span {
+            @include font-style(24px);
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+            &.next {
+                color: green;
+            }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+            &:not(:last-child) {
+                margin-right: 12px;
+            }
+        }
+    }
 
-a {
-  color: #42b983;
+    input {
+        width: 100%;
+        box-sizing: border-box;
+        padding: 5px 15px;
+        margin-top: 15px;
+        @include font-style(24px);
+
+    }
 }
 </style>

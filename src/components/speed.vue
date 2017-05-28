c0c3a2a07e0b3cca4d4ab88a0fbf8f3f57bb1236
Wahid Rahim <wahidrahim@gmail.com>
2017-05-27 23:39:54 -0400

    time and speed components

---
+++ src/components/speed.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+    <div class="speed">
+        {{ speed }} WPM
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['speed']
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/timeElapsed.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+    <div class="time-elapsed">
+        {{ time | timeElapsed }}
+    </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+    props: ['time'],
+    filters: {
+        timeElapsed: function(sec) {
+            return moment(`2016-06-12 00:00:00`).add(sec, 'seconds').format('HH:mm:ss')
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>

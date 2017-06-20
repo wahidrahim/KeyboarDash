c7607d4fcb5fe46355fda59d5865c74d890aaec2
Wahid Rahim <Wahid Rahim>
2017-06-20 17:00:24 -0400

    WIP - typescript files

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-import SinglePlayer from './pages/SinglePlayer.vue'
-import Scores from './pages/Scores.vue'
-
-export default [
-  { path: '/', component: SinglePlayer },
-  { path: '/scores', component: Scores }
-]
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
@@ -41,7 +41,8 @@ module.exports = {
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
-    }
+    },
+    extensions: ['.ts', '.js']
   },
   devServer: {
     historyApiFallback: true,

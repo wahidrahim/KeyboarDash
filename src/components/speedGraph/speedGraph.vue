ab8df11e2cbc558b97ecc2cb4ab1f6591e937845
Wahid Rahim <wahidrahim@gmail.com>
2017-06-23 16:11:13 -0400

    style update - checkers

--- src/components/speedGraph/speedGraph.vue
+++ src/components/speedGraph/speedGraph.vue
@@ -3,7 +3,7 @@
     <svg class="graph" :width="width" :height="height">
       <defs>
         <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-          <stop offset="0%" style="stop-color:rgba(236,100,75, 0.9);stop-opacity:1" />
+          <stop offset="0%" style="stop-color:rgba(231, 76, 60, 0.9);stop-opacity:1" />
           <stop offset="100%" style="stop-color:rgba(245, 215, 110, 0.9);stop-opacity:1" />
         </linearGradient>
       </defs>
